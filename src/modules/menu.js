a5f0c85556eeb38a13ef199352d40bfac07f752b
MYuPopov <beemax@bk.ru>
2022-01-15 02:09:06 +0300

    burger menu

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,6 +3,7 @@
 const menu = () => {
 	const headerContactsArrowBtn = document.querySelector('.header-contacts__arrow');
 	const menuIcon = document.querySelector('.menu__icon');
+	const closeMenu = document.querySelector('.close-menu');
 
 	headerContactsArrowBtn.addEventListener('click', () => {
 		const phoneNumberAccord = document.querySelector('.header-contacts__phone-number-accord a');
@@ -10,8 +11,14 @@ const menu = () => {
 		phoneNumberAccord.style.opacity = '100%';
 	});
 
-	menuIcon.addEventListener('click', (e) => {
-		console.log('click');
+	menuIcon.addEventListener('click', () => {
+		const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+		popupDialogMenu.style.transform = 'translate3d(0, 0, 0)';
+	});
+
+	closeMenu.addEventListener('click', () => {
+		const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+		popupDialogMenu.style.transform = 'translate3d(645px, 0, 0)';
 	});
 };
 export default menu;
