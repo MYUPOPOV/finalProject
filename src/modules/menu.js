149023b4e371be4f9b5beb06751c2a8ffea61775
MYuPopov <beemax@bk.ru>
2022-01-15 19:28:06 +0300

    Added Адаптив для меню

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -4,6 +4,31 @@ const menu = () => {
 	const headerContactsArrowBtn = document.querySelector('.header-contacts__arrow');
 	const menuIcon = document.querySelector('.menu__icon');
 	const closeMenu = document.querySelector('.close-menu');
+	const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+
+	const menuAnimationOn = () => {
+		const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+		popupDialogMenu.style.transition = '0s';
+
+		if (window.screen.width >= 576) {
+			popupDialogMenu.style.transform = 'translate3d(645px, 0, 0)';
+		}
+		if (window.screen.width < 576) {
+			popupDialogMenu.style.transform = 'translate3d(0, -1040px, 0)';
+		}
+		popupDialogMenu.style.transition = '1s';
+		popupDialogMenu.style.transform = 'translate3d(0, 0, 0)';
+	};
+
+	const menuAnimationOff = () => {
+		const popupDialogMenu = document.querySelector('.popup-dialog-menu');
+		if (window.screen.width >= 576) {
+			popupDialogMenu.style.transform = 'translate3d(645px, 0, 0)';
+		}
+		if (window.screen.width < 576) {
+			popupDialogMenu.style.transform = 'translate3d(0, -1040px, 0)';
+		}
+	};
 
 	headerContactsArrowBtn.addEventListener('click', () => {
 		const phoneNumberAccord = document.querySelector('.header-contacts__phone-number-accord a');
@@ -12,13 +37,18 @@ const menu = () => {
 	});
 
 	menuIcon.addEventListener('click', () => {
-		const popupDialogMenu = document.querySelector('.popup-dialog-menu');
-		popupDialogMenu.style.transform = 'translate3d(0, 0, 0)';
+		menuAnimationOn();
 	});
 
 	closeMenu.addEventListener('click', () => {
-		const popupDialogMenu = document.querySelector('.popup-dialog-menu');
-		popupDialogMenu.style.transform = 'translate3d(645px, 0, 0)';
+		menuAnimationOff();
 	});
+
+	if (window.screen.width >= 576) {
+		popupDialogMenu.style.transform = 'translate3d(645px, 0, 0)';
+	}
+	if (window.screen.width < 576) {
+		popupDialogMenu.style.transform = 'translate3d(0, -1040px, 0)';
+	}
 };
 export default menu;
